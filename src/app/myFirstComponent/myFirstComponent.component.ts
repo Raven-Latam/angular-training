d6a28a9d153dd49ad47a81db32adb781b061f5ec
Jason Estrada <jason.estrada@raven.inc>
2025-04-16 13:23:08 -0600

    myFirstComponent con Angular Material

--- src/app/myFirstComponent/myFirstComponent.component.ts
+++ src/app/myFirstComponent/myFirstComponent.component.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-my-first-component',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './myFirstComponent.component.html',
   styleUrl: './myFirstComponent.component.css',
 })
@@ -17,3 +19,17 @@ export class MyFirstComponentComponent {
     this.isShow.set(false);
   }
 }
+
+// import { Component } from '@angular/core';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+// @Component ({
+//   selector: 'app-my-first-component',
+//   templateUrl: './myFirstComponent.component.html',
+//   imports: [
+//     MatSlideToggleModule,
+//   ]
+// })
+// class MyFirstComponentComponent {
+
+// }
